59fc623f95c0decb60e81344a87a0221708302be
Andrew Pitt <apitt@redhat.com>
2019-12-01 15:27:58 -0500

    Added init script.

---
+++ init-argo.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+# Based on: https://blog.openshift.com/introduction-to-gitops-with-openshift/
+
+# Create namespace
+oc create namespace argocd
+
+# Apply the ArgoCD Install Manifest
+oc -n argocd apply -f https://raw.githubusercontent.com/argoproj/argo-cd/v1.2.5/manifests/install.yaml
+
+# Get the ArgoCD Server password
+ARGOCD_SERVER_PASSWORD=$(oc -n argocd get pod -l "app.kubernetes.io/name=argocd-server" -o jsonpath='{.items[*].metadata.name}')
+echo "Password: $ARGOCD_SERVER_PASSWORD"
+
+# Patch ArgoCD Server so no TLS is configured on the server (--insecure)
+PATCH='{"spec":{"template":{"spec":{"$setElementOrder/containers":[{"name":"argocd-server"}],"containers":[{"command":["argocd-server","--insecure","--staticassets","/shared/app"],"name":"argocd-server"}]}}}}'
+oc -n argocd patch deployment argocd-server -p $PATCH
+
+# Expose the ArgoCD Server using an Edge OpenShift Route so TLS is used for incoming connections
+oc -n argocd create route edge argocd-server --service=argocd-server --port=http --insecure-policy=Redirect
+
+# Create a new Project
+oc create -f projects/java-project.yaml
+
+# Create an Application
+oc create -f applications/java-application.yaml
