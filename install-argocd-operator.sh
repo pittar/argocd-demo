6c7e1b23b05ebf01b51224c78f34eb35ebb4458d
Andrew Pitt <apitt@redhat.com>
2019-12-13 14:14:07 -0500

    Playing with organization.

--- install-argocd-operator.sh
+++ install-argocd-operator.sh
@@ -9,19 +9,23 @@ oc create -f https://raw.githubusercontent.com/argoproj-labs/argocd-operator/mas
 oc create -f https://raw.githubusercontent.com/argoproj-labs/argocd-operator/master/deploy/role_binding.yaml
 # Until I figure out how configure the argocd role to be able to create namespacdes...
 oc adm policy add-cluster-role-to-user cluster-admin system:serviceaccount:argocd:argocd-application-controller
+sleep 3
 
 echo "Add the Argo CD CRDs."
 oc create -f https://raw.githubusercontent.com/argoproj-labs/argocd-operator/master/deploy/argo-cd/argoproj_v1alpha1_appproject_crd.yaml
 oc create -f https://raw.githubusercontent.com/argoproj-labs/argocd-operator/master/deploy/argo-cd/argoproj_v1alpha1_application_crd.yaml
+sleep 3
 
 echo "Add the Argo CD operator CRD."
 oc create -f https://raw.githubusercontent.com/argoproj-labs/argocd-operator/master/deploy/crds/argoproj_v1alpha1_argocd_crd.yaml
+sleep 3
 
 echo "Listing CRDs.  There should be three!"
 oc get crd | grep argo
-
+sleep 3
 echo "Deploy the operator."
 oc create -f https://raw.githubusercontent.com/argoproj-labs/argocd-operator/master/deploy/operator.yaml
+sleep 3
 
 echo "Create an Argo CD instance."
 oc create -f https://raw.githubusercontent.com/pittar/argocd-demo/master/deploy/argocd.yaml
